80aac3cfc644647f948d1ee0d06d974aa0056f43
he.wj <1174466499@qq.com>
2017-12-10 21:32:53 +0800

    eureka-server 注册中心的初始化工程

---
+++ src/main/java/com/hewenjian/Application.java
@@ -0,0 +1,19 @@
+package com.hewenjian;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 启动类
+ *
+ * @author he.wj
+ * @create 2017-12-10 20:40
+ **/
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
